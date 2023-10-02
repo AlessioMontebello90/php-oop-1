de73cb4fff7ecd1e1e0486c70486197366e13651
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-10-02 10:30:33 +0200

    fix

--- index.php
+++ index.php
@@ -4,8 +4,13 @@ require_once __DIR__ . '/Models/Genre.php';
 require_once __DIR__ . '/Models/Movie.php';
 
 $thriller = new Genre('Thriller');
+$romantic = new Genre('Romantic');
 
 $memento = new Movie('Memento', '2:30', 'http//...', $thriller);
 $titanic = new Movie('Titanic', '2:30', 'http//...', $romantic);
 
+
+
 echo $memento->getDetail();
+
+echo $titanic->getDetail();
