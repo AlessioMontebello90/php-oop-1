cd3bc21b4be6c73e9f132178461f88a6e202a40f
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-10-02 09:49:12 +0200

    add title

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ require_once __DIR__ . '/Models/Movie.php';
 $thriller = new Genre('Thriller');
 
 $memento = new Movie('Memento', '2:30', 'http//...', $thriller);
+$titanic = new Movie('Titanic', '2:30', 'http//...', $romantic);
 
 echo $memento->getDetail();
