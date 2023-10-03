e7595028ee81a0c2157a816c1b4c05f06e69d475
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-10-03 13:46:23 +0200

    add construct in php

---
+++ models/Movie.php
@@ -0,0 +1,37 @@
+<?php
+class Movie {
+    public $name;
+    public $year;
+    public $genres;
+    public $mainActors;
+    public $adultOnly;
+    function __construct(string $name, int $year, array $genres, Actors $mainActors, bool $adultOnly){
+
+        $this->name = $name;
+        $this->year = $year;
+        $this->genres = $genres;
+        $this->mainActors = $mainActors;
+        $this->adultOnly = $adultOnly;
+
+
+    }
+    public function setAdult(){
+        if($this->adultOnly == true){
+            return  'Visione consigliata solo ai Maggiorenni';
+            
+        } else {
+             return  'Visione consigliata a tutti';
+           
+        }
+    }
+
+    public function getGenres (){
+
+        $array_stringa = implode(',',$this->genres);
+        return $array_stringa;
+    }
+
+
+
+
+}
